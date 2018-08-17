6e40cdd75c6b864288f9689b373377b1f768961b
Nana Yeboah <yeboahnanaosei@gmail.com>
2018-08-17 10:50:53 +0000

    Refactor. Add redundant comments

--- backend/add-new-quote.php
+++ backend/add-new-quote.php
@@ -1,13 +1,23 @@
 <?php
+// Path to the JSON file (obviously)
 $jsonFile = '../js/primary-quotes.json';
+
+// Convert the contents into an array. Just like JSON.parse() in JS
 $quotes = json_decode(file_get_contents($jsonFile), true);
-$newData = [
+
+// This is just dumb data to represent the data that will be submitted
+// by whoever is adding a new quote. This might need some validation or
+// sanitization.
+$newQuote = [
         "img" => "someimgae",
         "quote" => "some powerful quote from nana",
         "author" => "Nana Osei",
-        "bgColor" => "somecolor"
+        "bgColor" => "someStrangeColor"
 ];
 
-array_push($quotes, $newData);
-var_dump($quotes);
-var_dump(file_put_contents($jsonFile, json_encode($quotes, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)));
+// Add the new quote to the quotes
+$quotes[] = $newQuote;
+
+// Encode the new quotes into JSON format and save the file
+$quotes = json_encode($quotes, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+file_put_contents($jsonFile, $quotes, LOCK_EX);
